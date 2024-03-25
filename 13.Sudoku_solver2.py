87fc0d5686c3460ca2e3ae98872d103bd923f303
SHypoleac <michal.wolonciej@interia.pl>
2024-03-25 20:27:07 +0100

    nowy plik:       13.Sudoku_solver2.py

---
+++ 13.Sudoku_solver2.py
@@ -0,0 +1,54 @@
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+def is_valid(board, row, col, num):
+    # Check the row
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    # Check the column
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    # Check the box
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, i, j, num):
+                        board[i][j] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[i][j] = 0
+                return False
+    return True
+
+if solve_sudoku(sudoku_board):
+    print("Solution found:")
+    for i in range(9):
+        for j in range(9):
+            print(sudoku_board[i][j], end=" ")
+        print()
+else:
+    print("No solution exists.")
